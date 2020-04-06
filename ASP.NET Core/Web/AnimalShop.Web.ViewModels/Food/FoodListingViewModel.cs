ebae2968600aa0e4cd2bc71369dc00e3d0d113c2
Hristo <i4kowee@gmail.com>
2020-04-06 14:06:39 +0300

    Change project location

---
+++ ASP.NET Core/Web/AnimalShop.Web.ViewModels/Food/FoodListingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalShop.Web.ViewModels.Food
+{
+    using System.Collections.Generic;
+
+    public class FoodListingViewModel
+    {
+        public int Count { get; set; }
+
+        public IEnumerable<FoodViewModel> Food { get; set; }
+    }
+}
