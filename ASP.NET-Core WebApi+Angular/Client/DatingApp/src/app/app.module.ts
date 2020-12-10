2bc61c34f344633b1eaea6e2a414d7f7088fdf7d
Hristo <i4kowee@gmail.com>
2020-12-10 22:43:37 +0200

    login response from server

--- ASP.NET-Core WebApi+Angular/Client/DatingApp/src/app/app.module.ts
+++ ASP.NET-Core WebApi+Angular/Client/DatingApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { NavComponent } from './nav/nav.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
