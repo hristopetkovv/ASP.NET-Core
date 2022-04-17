57a4844ccf7161a4872d464476d7637624e7b696
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2022-04-17 19:56:21 +0300

    create repositories and email service

---
+++ ASP.NET-Core Microservices/aspnetrun-microservices/Services/Ordering/Ordering.Infrastucture/Mail/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastucture;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastucture.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            EmailSettings = emailSettings.Value;
+            Logger = logger;
+        }
+
+        public EmailSettings EmailSettings { get; }
+        public ILogger<EmailService> Logger { get; }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(this.EmailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = this.EmailSettings.FromAddress,
+                Name = this.EmailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            this.Logger.LogInformation("Email sent.");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            this.Logger.LogError("Email sending failed.");
+            return false;
+        }
+    }
+}
---
+++ ASP.NET-Core Microservices/aspnetrun-microservices/Services/Ordering/Ordering.Infrastucture/Repositories/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastucture.Persistence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastucture.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        protected readonly OrderContext dbContext;
+
+        public OrderRepository(OrderContext dbContext)
+            : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await dbContext.Orders
+                .Where(o => o.UserName == userName)
+                .ToListAsync();
+
+            return orderList;
+        }
+    }
+}
