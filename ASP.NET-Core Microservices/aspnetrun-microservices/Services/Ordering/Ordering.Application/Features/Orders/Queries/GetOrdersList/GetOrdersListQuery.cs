b444f17bf7a9d9b83877386b75e1019ca4934ad8
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2022-04-17 12:09:31 +0300

    create get orders list query

---
+++ ASP.NET-Core Microservices/aspnetrun-microservices/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public GetOrdersListQuery(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public string UserName { get; }
+    }
+}
