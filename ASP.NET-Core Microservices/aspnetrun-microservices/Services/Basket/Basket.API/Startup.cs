ee577e1476a8e22505b5f7d00f236a7723b5c0f5
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2022-04-19 21:20:12 +0300

    add mass transit and rabbit mq in basket api

--- ASP.NET-Core Microservices/aspnetrun-microservices/Services/Basket/Basket.API/Startup.cs
+++ ASP.NET-Core Microservices/aspnetrun-microservices/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -31,6 +32,12 @@ namespace Basket.API
             services.AddScoped<DiscountGrpcService>();
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+
+            services.AddMassTransit(config => {
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
