28e59ff0cd2b68bf6e7ccdf1f3c5221f89dbe173
Hristo Petkov <45469690+hristopetkovv@users.noreply.github.com>
2022-04-20 22:41:43 +0300

    configure masstransit and rabbitmq to order api consumer

--- ASP.NET-Core Microservices/aspnetrun-microservices/Services/Ordering/Ordering.API/Startup.cs
+++ ASP.NET-Core Microservices/aspnetrun-microservices/Services/Ordering/Ordering.API/Startup.cs
@@ -1,9 +1,12 @@
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Ordering.API.EventBusConsumer;
 using Ordering.Application;
 using Ordering.Infrastucture;
 
@@ -18,11 +21,22 @@ namespace Ordering.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationServices();
             services.AddInfrastructureServices(this.Configuration);
+
+            services.AddMassTransit(config => {
+                config.AddConsumer<BasketCheckoutConsumer>();
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+
+                    cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+                    {
+                        c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+                    });
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -31,7 +45,6 @@ namespace Ordering.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
